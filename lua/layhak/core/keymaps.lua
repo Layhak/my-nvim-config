bc03478791afaf116630fc0baea5f868df8e476f
Heng <layhakheng@gmail.com>
2024-08-19 14:39:07 +0700

    chaning alpha icon

--- lua/layhak/core/keymaps.lua
+++ lua/layhak/core/keymaps.lua
@@ -12,8 +12,9 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Go to Lazy panel" })
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
